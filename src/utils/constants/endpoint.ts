9352788f0dcc4bb9c37f1bab5ab776be421566cd
abiyyu03 <abiyyu652@gmail.com>
2023-06-10 10:47:18 +0700

    create ENDPOINT constants

---
+++ src/utils/constants/endpoint.ts
@@ -0,0 +1,18 @@
+const API_KEY = import.meta.env.VITE_MOVIE_API_KEY;
+
+const BASE_URL = `https://api.themoviedb.org/3`;
+
+const ENDPOINT = {
+    POPULAR:`${BASE_URL}/movie/popular?api_key=${API_KEY}`,
+    TRENDING:`${BASE_URL}/trending/movie/day?api_key=${API_KEY}`,
+    NOW_PLAYING:`${BASE_URL}/movie/now_playing?api_key=${API_KEY}`,
+    TOP_RATED:`${BASE_URL}/movie/top_rated?api_key=${API_KEY}`,
+    DETAIL(id:any){
+        return `${BASE_URL}/movie/${id}?api_key=${API_KEY}&append_to_response=videos`
+    },
+    RECOMMENDATION(id:any){
+        return `${BASE_URL}/movie/${id}/recommendations?api_key=${API_KEY}`
+    }
+}
+
+export default ENDPOINT;
